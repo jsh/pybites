681c89668a65cd8b3fa776f97f1726843ef3f516
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-01-16 09:35:19 -0700

    Use Bio module
    Two tests now failing because of ambiguous bases.

--- 306/translate_cds.py
+++ 306/translate_cds.py
@@ -1,7 +1,7 @@
 """Use translation table to translate coding sequence to protein."""
 # TODO: Add imports
 
-import Bio.Seq  # type: ignore
+from Bio.Seq import Seq # type: ignore
 from Bio.Data import CodonTable
 
 # Note on Bio.Seq table ids: These can be found in the
@@ -17,13 +17,7 @@ def translate_cds(cds: str, translation_table: str) -> str:
     :return: str: Protein sequence
     """
     table = CodonTable.unambiguous_dna_by_name[translation_table]
-    assert cds[:3] in table.start_codons
-    assert cds[-3:] in table.stop_codons
-
-    aa_list =[]
-    for first in range(0, len(cds)-3, 3):
-        codon = cds[first:first+3]
-        amino_acid = table.forward_table[codon]
-        aa_list.append(amino_acid)
-        protein = "".join(aa_list)
-    return protein
+    cds = "".join(cds.split())  # clean out whitespace
+    coding_dna = Seq(cds)
+    protein = coding_dna.translate(table, cds=True, to_stop=True)
+    return str(protein)
