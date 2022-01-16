28adfba765dfce27517d27e74b8172b96ae0f8fe
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-01-16 07:20:51 -0700

    janitorial work

--- 306/translate_cds.py
+++ 306/translate_cds.py
@@ -1,11 +1,13 @@
+"""Use translation table to translate coding sequence to protein."""
 # TODO: Add imports
 
 
 def translate_cds(cds: str, translation_table: str) -> str:
-    """
+    """Translate coding sequence to protein.
+
     :param cds: str: DNA coding sequence (CDS)
-    :param translation_table: str: translation table as defined in Bio.Seq.Seq.CodonTable.ambiguous_generic_by_name
+    :param translation_table: str: translation table
+        as defined in Bio.Seq.Seq.CodonTable.ambiguous_generic_by_name
     :return: str: Protein sequence
     """
-
     # TODO: Put your code here
