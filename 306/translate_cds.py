c62505726e19f53fc79be23239fe19b2ed22196f
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-01-16 08:53:56 -0700

    hand translate

--- 306/translate_cds.py
+++ 306/translate_cds.py
@@ -1,6 +1,12 @@
 """Use translation table to translate coding sequence to protein."""
 # TODO: Add imports
 
+import Bio.Seq  # type: ignore
+from Bio.Data import CodonTable
+
+# Note on Bio.Seq table ids: These can be found in the
+# Seq.CodonTable.ambiguous_generic_by_name variable
+
 
 def translate_cds(cds: str, translation_table: str) -> str:
     """Translate coding sequence to protein.
@@ -10,4 +16,14 @@ def translate_cds(cds: str, translation_table: str) -> str:
         as defined in Bio.Seq.Seq.CodonTable.ambiguous_generic_by_name
     :return: str: Protein sequence
     """
-    # TODO: Put your code here
+    table = CodonTable.unambiguous_dna_by_name[translation_table]
+    assert cds[:3] in table.start_codons
+    assert cds[-3:] in table.stop_codons
+
+    aa_list =[]
+    for first in range(0, len(cds)-3, 3):
+        codon = cds[first:first+3]
+        amino_acid = table.forward_table[codon]
+        aa_list.append(amino_acid)
+        protein = "".join(aa_list)
+    return protein
