c0e6b0654af87b5442e895be91b5c7b0416e766c
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-01-19 17:36:26 -0700

    pass all tests

--- 303/unique_genes.py
+++ 303/unique_genes.py
@@ -25,30 +25,34 @@ def convert_to_unique_genes(filename_in, filename_out):
 
     returns None
     """
-    try:
-        with gzip.open(filename_in, "rt") as f_in, open(
-            filename_out, "w", encoding="utf-8"
-        ) as f_out:
+    if filename_in.endswith(".gz"):
+        with gzip.open(filename_in, "rt") as f_in:
             all = f_in.readlines()
+        filename_in = filename_in[:-3]
+        with open(filename_in, "w", encoding="utf-8") as f_out:
             f_out.writelines(all)
-        os.replace(filename_out, filename_in)
-    except gzip.BadGzipFile:
-        pass
     pat = re.compile(r"(\S+)\s\[locus_tag=(.*)]")
     record_list = defaultdict(list)
     gene_list = {}
     for record in SeqIO.parse(filename_in, "fasta"):
-        gene = pat.match(record.description)[1]
+        gene = pat.match(record.description)[1].lower()
         locus_tag = pat.match(record.description)[2]
         seq = str(record.seq).upper()
         record_list[seq].append(locus_tag)
+        if seq in gene_list and gene_list[seq] != gene:
+            msg = f"Gene names differ between entries: '{gene_list[seq]}' vs. '{gene}'"
+            raise NameError(msg)
         gene_list[seq] = gene
     record_list = sorted(record_list.items(), key=ntags, reverse=True)
-    with open(filename_out, "w", encoding="utf-8") as f_out:
-        for record in record_list:
-            seq, locus_tags = record
-            plural = "s" if len(locus_tags) > 1 else ""
-            locus_tags = ",".join(locus_tags)
-            gene = gene_list[seq]
-            f_out.write(f">{gene} [locus_tag{plural}={locus_tags}]\n")
-            f_out.write(f"{seq}\n")
+    if filename_out.endswith(".gz"):
+        f_out = gzip.open(filename_out, "wt")
+    else:
+        f_out= open(filename_out, "w", encoding="utf-8")
+
+    for record in record_list:
+        seq, locus_tags = record
+        plural = "s" if len(locus_tags) > 1 else ""
+        locus_tags = ",".join(locus_tags)
+        gene = gene_list[seq]
+        f_out.write(f">{gene} [locus_tag{plural}={locus_tags}]\n")
+        f_out.write(f"{seq}\n")
