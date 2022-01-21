e1ed0bb0577324a09b46b6ee738eecf5a6bfcf67
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-01-21 06:12:36 -0700

    submission Bite 238 @ codechalleng.es

--- 238/test_fibonacci.py
+++ 238/test_fibonacci.py
@@ -1,3 +1,61 @@
+import pytest
+
 from fibonacci import fib
 
-# write one or more pytest functions below, they need to start with test_
+
+def test_base_case() -> None:
+    """Test 0, 1 terminating cases."""
+    assert fib(0) == 0
+    assert fib(1) == 1
+
+
+@pytest.mark.parametrize(
+    "arg, expected",
+    [
+        (2, 1),
+        (3, 2),
+        (4, 3),
+        (5, 5),
+        (20, 6765),
+        (True, 1),
+        (False, 0),
+    ],
+)
+def test_larger_cases(arg, expected) -> None:
+    """Test a few other integers."""
+    assert fib(arg) == expected
+
+def test_no_arg():
+    """Forget to pass an argument."""
+    with pytest.raises(TypeError):
+        assert fib()
+
+
+@pytest.mark.parametrize(
+    "bad_value, error",
+    [
+        (-1, ValueError),
+        (3.14, ValueError),
+    ]
+)
+def test_handled_error(bad_value, error) -> None:
+    """Test caught error condition."""
+    with pytest.raises(error):
+        assert fib(bad_value)
+
+
+@pytest.mark.parametrize(
+    "bad_value, error",
+    [
+        ("foozbaz", TypeError),
+        ((6, 9), TypeError),
+        (["a", "b", "c"], TypeError),
+        ({"x": 1}, TypeError),
+        ({"x", "y"}, TypeError),
+    ]
+)
+def test_unhandled_error(bad_value, error) -> None:
+    """Test other bad input."""
+    with pytest.raises(error):
+        assert fib(bad_value)
+
