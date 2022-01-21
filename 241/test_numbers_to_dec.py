87694b6194dffc49c07e2293e12fa8207c1962e2
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-01-21 07:23:03 -0700

    de-dos

--- 241/test_numbers_to_dec.py
+++ 241/test_numbers_to_dec.py
@@ -1,3 +1,34 @@
+"""Bite 241. Write tests for list_to_decimal."""
+from typing import Any, List
+
 import pytest
 
 from numbers_to_dec import list_to_decimal
+
+
+@pytest.mark.parametrize(
+    "numlist, expected_output",
+    [
+        ([0, 4, 2, 8], 428),
+        ([1, 2], 12),
+        ([3], 3),
+    ],
+)
+def test_good_input(numlist: List[int], expected_output: int) -> None:
+    """Good list returns correct answer."""
+    assert list_to_decimal(numlist) == expected_output
+
+
+@pytest.mark.parametrize(
+    "numlist, exception",
+    [
+        ([6, 4, 2, True], TypeError),
+        ([-3, 12], ValueError),
+        ([3.6, 4, 1], TypeError),
+        (["4", 5, 3, 1], TypeError),
+    ],
+)
+def test_exception(numlist: List[Any], exception: type) -> None:
+    """Bad list raises correct exception."""
+    with pytest.raises(exception):
+        assert list_to_decimal(numlist)
