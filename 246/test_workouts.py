8bce64fdc2541d0b05f7fe8c458d6d28c4c4f397
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-01-24 14:04:50 -0700

    submission Bite 246 @ codechalleng.es

--- 246/test_workouts.py
+++ 246/test_workouts.py
@@ -2,20 +2,21 @@
 
 import pytest
 
-from workouts import print_workout_days, WORKOUTS
+from workouts import print_workout_days
 
 
-@pytest.mark.parametrize (
-	"selector, expected_output",
-	[
-	("foo", "No matching workout\n"),
-	("#2", "Thu, Fri\n"),
-	("lower", "Tue, Fri\n"),
-	("card", "Wed\n")
-	]
+@pytest.mark.parametrize(
+    "selector, expected_output",
+    [
+        ("foo", "No matching workout\n"),
+        ("#2", "Thu, Fri\n"),
+        ("lower", "Tue, Fri\n"),
+        ("card", "Wed\n"),
+        (" ", "Mon, Tue, Wed, Thu, Fri\n"),
+    ],
 )
 def test_print_workout_days(selector: str, expected_output: str, capsys) -> None:
-	"""Unit-test print_workout_days."""
-	print_workout_days(selector, WORKOUTS)
-	captured = capsys.readouterr()
-	assert captured.out == expected_output
+    """Unit-test print_workout_days."""
+    print_workout_days(selector)
+    captured = capsys.readouterr()
+    assert captured.out == expected_output
