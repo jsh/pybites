57c9f70b8a031b43f8cc3a0edbca54d19093cfe3
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-01-25 08:56:02 -0700

    sunny day

--- 239/fizzbuzz.py
+++ 239/fizzbuzz.py
@@ -1,9 +1,12 @@
+"""Bite 239. Test FizzBuzz."""
+
+
 def fizzbuzz(num):
+    """Test divisibility."""
     if num % 3 == 0 and num % 5 == 0:
-        return 'Fizz Buzz'
-    elif num % 3 == 0:
-        return 'Fizz'
-    elif num % 5 == 0:
-        return 'Buzz'
-    else:
-        return num
+        return "Fizz Buzz"
+    if num % 3 == 0:
+        return "Fizz"
+    if num % 5 == 0:
+        return "Buzz"
+    return num
--- 239/test_fizzbuzz.py
+++ 239/test_fizzbuzz.py
@@ -1,3 +1,37 @@
+"""Bite 239. Test FizzBuzz."""
+
+from typing import Any
+
+import pytest
+
 from fizzbuzz import fizzbuzz
 
 # write one or more pytest functions below, they need to start with test_
+
+
+@pytest.mark.parametrize(
+    "integer, expected_output",
+    [
+        (3, "Fizz", "divisible by three"),
+        (5, "Buzz", "divisible by five"),
+        (15, "Fizz Buzz", "divisible by both"),
+        (7, 7, "divisible by neither"),
+        (0, "Fizz Buzz", "zero"),
+        (-5, "Buzz", "negative number"),
+    ],
+)
+def test_fizzbuzz(integer: int, expected_output: str) -> None:
+    """Unit-test fizzbuzz."""
+    assert fizzbuzz(integer) == expected_output
+
+
+@pytest.mark.parametrize(
+    "bad_input, expected_exception",
+    [
+        ("foo", TypeError),
+    ],
+)
+def test_invalid_input(bad_input: Any, expected_exception) -> None:
+    """Check bad input."""
+    with pytest.raises(expected_exception):
+        assert fizzbuzz(bad_input)
