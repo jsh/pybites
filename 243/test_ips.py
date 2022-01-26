ce83d00c6fa18c8bd2598f273dee27e625e7a0f9
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-01-26 11:21:54 -0700

    janitorial work

--- 243/test_ips.py
+++ 243/test_ips.py
@@ -1,11 +1,11 @@
 """Bite 243. Test code that parses JSON and IP ranges."""
 
-import os
 import json
+import os
 from ipaddress import IPv4Network
 from pathlib import Path
-from urllib.request import urlretrieve
 from typing import List
+from urllib.request import urlretrieve
 
 import pytest
 
@@ -19,16 +19,20 @@ IP = IPv4Network("192.0.2.8/29")
 
 @pytest.fixture(scope="module")
 def json_file() -> Path:
-    """Import data into tmp folder"""
+    """Import data into tmp folder."""
     urlretrieve(URL, PATH)
     return PATH
 
+
 @pytest.fixture(scope="module")
 def service_ranges(json_file) -> List[ServiceIPRange]:
+    """List of services ranges from data."""
     return parse_ipv4_service_ranges(json_file)
+
 
 @pytest.fixture(scope="module")
 def dummy_json() -> str:
+    """Completely bogus JSON data."""
     return '{ \
       "syncToken": "1575592390", \
       "createDate": "2019-12-06-00-33-10", \
@@ -39,20 +43,26 @@ def dummy_json() -> str:
           "service": "MYTEST" \
         } \
       ] \
-    }' 
-    
+    }'
+
+
 def test_class() -> None:
+    """Unit-test the class itself."""
     sipr = ServiceIPRange(
         service="MYSERVICE",
         region="MYREGION",
-        cidr="1.2.3.4/69",
+        cidr=IPv4Network("1.2.3.4/32"),
     )
     assert isinstance(sipr, ServiceIPRange)
     assert sipr.service == "MYSERVICE"
-    assert repr(sipr) == "ServiceIPRange(service='MYSERVICE', region='MYREGION', cidr='1.2.3.4/69')"
-    assert str(sipr) == "1.2.3.4/69 is allocated to the MYSERVICE service in the MYREGION region"
+    assert (
+        str(sipr)
+        == "1.2.3.4/32 is allocated to the MYSERVICE service in the MYREGION region"
+    )
+
 
 def test_parse_ipv4_service_ranges(dummy_json):
+    """Unit-test parse_ipv4_service_ranges."""
     data = json.loads(dummy_json)
     prefixes = data["prefixes"]
     prefix = prefixes[0]
@@ -69,32 +79,35 @@ def test_parse_ipv4_service_ranges(dummy_json):
 
     assert parse_ipv4_service_ranges(json_file) == service_list
 
+
 @pytest.mark.parametrize(
     "input_ip, expected_output",
     [
-       ("54.250.0.0",
+        (
+            "54.250.251.0",
             [
-            "54.250.0.0/16 is allocated to the AMAZON service in the ap-northeast-1 region",
-            "54.250.0.0/16 is allocated to the EC2 service in the ap-northeast-1 region",
-            "54.250.251.0/24 is allocated to the WORKSPACES_GATEWAYS service in the ap-northeast-1 region",
-            ]
+                "54.250.0.0/16 is allocated to the AMAZON service in the ap-northeast-1 region",
+                "54.250.0.0/16 is allocated to the EC2 service in the ap-northeast-1 region",
+                "54.250.251.0/24 is allocated to the WORKSPACES_GATEWAYS service in the ap-northeast-1 region",
+            ],
         )
-    ]
+    ],
 )
 def test_get_aws_service_range(input_ip, expected_output, service_ranges) -> None:
+    """Unit-test get_aws_service_range."""
     services = get_aws_service_range(input_ip, service_ranges)
     service_list = []
     for service in services:
         service_list.append(str(service))
-        
-    expected_output = service_list
+    assert expected_output == service_list
+
 
 def test_bad_ip(service_ranges) -> None:
-    """Bad value."""
+    """Unit-test bad IP value throws correct exception."""
     with pytest.raises(ValueError):
         get_aws_service_range("foo", service_ranges)
 
 
 def test_nonexistent_ip(service_ranges) -> None:
-    """Bad value."""
+    """IP not in services returns empty list."""
     assert get_aws_service_range("127.0.0.1", service_ranges) == []
