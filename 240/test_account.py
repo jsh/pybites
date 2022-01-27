5dfbaeee555cc1834a1545b570d2163da53d7318
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-01-27 11:33:58 -0700

    Initial import + janitorial

---
+++ 240/test_account.py
@@ -0,0 +1,8 @@
+"""Bite 240. Write tests for an Account class."""
+
+from account import Account
+
+
+# write your pytest functions below, they need to start with test_
+def test_dummy() -> None:
+    pass
