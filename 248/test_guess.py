336b96472d5d9dfd6947b4ab9309d394ad6f0a72
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-01-30 08:17:26 -0700

    Improved _validate() check.
    Still stuck on mocking input()

--- 248/test_guess.py
+++ 248/test_guess.py
@@ -7,12 +7,11 @@ from guess import MAX_NUMBER, GuessGame, InvalidNumber
 
 # write test code to reach 100% coverage and a 100% mutpy score
 
-
-def test_dummy() -> None:
-    pass
-
+def my_input():
+	return 2
 
 def test_init() -> None:
+    """Unit-test __init__()."""
     game = GuessGame(1)
     assert game.secret_number == 1
     assert game.max_guesses == 5
@@ -28,11 +27,15 @@ def test_init() -> None:
     ],
 )
 def test_validate(secret_number, err_msg) -> None:
-    with pytest.raises(InvalidNumber) as exc:
+    """Unit-test _validate()."""
+    with pytest.raises(InvalidNumber, match=err_msg) as exc:
         game = GuessGame(secret_number)
-    assert err_msg in str(exc)
 
-
-def test_call() -> None:
-    game = GuessGame(1)
-    # game()
+#@patch("guess.input", side_effect=[1, 3, 2])
+@patch("guess.input", my_input)
+#@patch("guess.input", side_effect=[1, 3, 2])
+def test_call(capsys) -> None:
+    """Unit-test __call__()."""
+    game = GuessGame(2)
+    game()
+    captured = capsys.readouterr()
