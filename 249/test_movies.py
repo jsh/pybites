337be7b8e95c9bc10a65bf2ba0a8b75467ac57d9
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-02 12:42:56 -0700

    Basic tests, which pass.

--- 249/test_movies.py
+++ 249/test_movies.py
@@ -11,6 +11,7 @@ from movies import MovieDb
 
 salt = "".join(random.choice(string.ascii_lowercase) for i in range(20))
 DB = os.path.join(os.getenv("TMP", "/tmp"), f"movies_{salt}.db")
+# DB = f"movies.db"  # TODO: remove
 # https://www.imdb.com/list/ls055592025/
 DATA = [
     ("The Godfather", 1972, 9.2),
@@ -28,8 +29,9 @@ TABLE = "movies"
 
 
 @pytest.fixture
-def db(request):
+def db():
     """Fixture to instantiate movie class."""
+    # TODO: restore db(request)
     # instantiate MovieDb class using above constants
     # do proper setup / teardown using MovieDb methods
     # https://docs.pytest.org/en/latest/fixture.html (hint: yield)
@@ -37,17 +39,14 @@ def db(request):
     # TODO: tear-down with drop_table
 
     database = MovieDb(DB, DATA, TABLE)
-    return database
-
-    def fin():
-        database.drop_table()
-
-    request.add_finalizer(fin)
+    database.init()
+    yield database
+    database.drop_table()
 
 
 def test_init(db):
     assert isinstance(db, MovieDb)
-    assert db.table == "movies"
+    assert db.table == TABLE
     assert isinstance(db.cur, sqlite3.Cursor)
     assert db.data == DATA
     assert isinstance(db.con, sqlite3.Connection)
@@ -58,12 +57,24 @@ def test_query(db):
     # TODO: lots of args, test them all
     datum = DATA[0]
     reply = db.query(*datum)
-    assert reply == Null
+    assert reply == [(1,) + datum]
+    assert len(db.query(year=1939)) == 2
+    assert len(db.query(score_gt=8.3)) == 5
+    assert len(db.query(title="Montenegro")) == 0
 
 
 def test_add(db):
     """Unit-test add."""
+    crumb = ("Crumb", 1994, 9.5)
+    db.add("Crumb", 1994, 9.5)
+    assert db.query(title="Crumb")[0][1:] == crumb
 
 
 def test_delete(db):
     """Unit-test delete."""
+    crumb = ("Crumb", 1994, 9.5)
+    db.add("Crumb", 1994, 9.5)
+    idx= db.query(title="Crumb")[0][0]
+    db.delete(idx)
+    assert db.query(title="Crumb") == []
+	
