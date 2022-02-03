dfaafa877241b670e8f574d9287c99320be77f17
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-02 22:06:26 -0700

    janatorial

--- 130/cars.py
+++ 130/cars.py
@@ -1,23 +1,29 @@
+"""Bite 130. Analyze some basic Car Data."""
+
 from collections import Counter
 
 import requests
 
-CAR_DATA = 'https://bites-data.s3.us-east-2.amazonaws.com/cars.json'
+CAR_DATA = "https://bites-data.s3.us-east-2.amazonaws.com/cars.json"
 
 # pre-work: load JSON data into program
 
-with requests.Session() as s:
-    data = s.get(CAR_DATA).json()
+with requests.Session() as session:
+    data = session.get(CAR_DATA).json()
 
 
-# your turn:
+# TODO: write the code
 def most_prolific_automaker(year):
-    """Given year 'year' return the automaker that released
-       the highest number of new car models"""
-    pass
+    """Find most prolific automaker of the year.
+
+    Given year 'year' return the automaker that released
+    the highest number of new car models
+    """
 
 
 def get_models(automaker, year):
-    """Filter cars 'data' by 'automaker' and 'year',
-       return a set of models (a 'set' to avoid duplicate models)"""
-    pass
+    """Get models for automakker and year.
+
+    Filter cars 'data' by 'automaker' and 'year',
+    return a set of models (a 'set' to avoid duplicate models)
+    """
