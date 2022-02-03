687092a4fd91bbf8b531d9c3263069dd531dd798
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-03 09:48:14 -0700

    submission Bite 130 @ codechalleng.es

--- 130/cars.py
+++ 130/cars.py
@@ -1,10 +1,9 @@
 """Bite 130. Analyze some basic Car Data."""
 
 from collections import Counter
+from typing import Set
 
 import requests
-
-from typing import Set
 
 CAR_DATA = "https://bites-data.s3.us-east-2.amazonaws.com/cars.json"
 
@@ -21,11 +20,20 @@ def most_prolific_automaker(year: int) -> str:
     Given year 'year' return the automaker that released
     the highest number of new car models
     """
+    year_data = [datum["automaker"] for datum in data if (datum["year"] == year)]
+    counted = Counter(year_data)
+    return counted.most_common()[0][0]
 
 
-def get_models(automaker: str, year: none) -> Set[str]:
-    """Get models for automakker and year.
+def get_models(automaker: str, year: int) -> Set[str]:
+    """Get models for automaker and year.
 
     Filter cars 'data' by 'automaker' and 'year',
     return a set of models (a 'set' to avoid duplicate models)
     """
+    filtered = [
+        datum["model"]
+        for datum in data
+        if datum["automaker"] == automaker and datum["year"] == year
+    ]
+    return set(filtered)
--- 130/test_cars.py
+++ 130/test_cars.py
@@ -1,47 +1,38 @@
-"""Bite 130. Analyze some basic Car Data."""
-
-from cars import get_models, most_prolific_automaker
+from cars import most_prolific_automaker, get_models
 
 
-def test_most_prolific_automaker_1999() -> None:
-    """Most prolific automaker of 1999."""
-    assert most_prolific_automaker(1999) == "Dodge"
+def test_most_prolific_automaker_1999():
+    assert most_prolific_automaker(1999) == 'Dodge'
 
 
-def test_most_prolific_automaker_2008() -> None:
-    """Most prolific automaker of 2008."""
-    assert most_prolific_automaker(2008) == "Toyota"
+def test_most_prolific_automaker_2008():
+    assert most_prolific_automaker(2008) == 'Toyota'
 
 
-def test_most_prolific_automaker_2013() -> None:
-    """Most prolific automaker of 2013."""
-    assert most_prolific_automaker(2013) == "Hyundai"
+def test_most_prolific_automaker_2013():
+    assert most_prolific_automaker(2013) == 'Hyundai'
 
 
-def test_get_models_volkswagen() -> None:
-    """VW models for 2008."""
-    models = get_models("Volkswagen", 2008)
+def test_get_models_volkswagen():
+    models = get_models('Volkswagen', 2008)
     # sets are unordered
     assert len(models) == 2
-    assert "Jetta" in models
-    assert "Rabbit" in models
+    assert 'Jetta' in models
+    assert 'Rabbit' in models
 
 
-def test_get_models_nissan() -> None:
-    """Nissan models 2000."""
-    assert get_models("Nissan", 2000) == {"Pathfinder"}
+def test_get_models_nissan():
+    assert get_models('Nissan', 2000) == {'Pathfinder'}
 
 
-def test_get_models_open() -> None:
-    """Try to get missing models."""
+def test_get_models_open():
     # not in data set
-    assert get_models("Opel", 2008) == set()
+    assert get_models('Opel', 2008) == set()
 
 
-def test_get_models_mercedes() -> None:
-    """Mercedes models 2007."""
-    models = get_models("Mercedes-Benz", 2007)
+def test_get_models_mercedes():
+    models = get_models('Mercedes-Benz', 2007)
     assert len(models) == 3
-    assert "SL-Class" in models
-    assert "GL-Class" in models
-    assert "CL-Class" in models
+    assert 'SL-Class' in models
+    assert 'GL-Class' in models
+    assert 'CL-Class' in models
