497cf146573ea529d29bb5f404c70a9a53d7cdd6
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-04 12:33:43 -0700

    submission Bite 18 @ codechalleng.es

--- 18/harry.py
+++ 18/harry.py
@@ -1,19 +1,33 @@
+#!/usr/bin/env python3
+"""Bite 18. Find the most common word."""
+
 import os
+import re
 import urllib.request
+from collections import Counter
 
 # data provided
 tmp = os.getenv("TMP", "/tmp")
-stopwords_file = os.path.join(tmp, 'stopwords')
-harry_text = os.path.join(tmp, 'harry')
+stopwords_file = os.path.join(tmp, "stopwords")
+harry_text = os.path.join(tmp, "harry")
 urllib.request.urlretrieve(
-    'https://bites-data.s3.us-east-2.amazonaws.com/stopwords.txt',
-    stopwords_file
+    "https://bites-data.s3.us-east-2.amazonaws.com/stopwords.txt", stopwords_file
 )
 urllib.request.urlretrieve(
-    'https://bites-data.s3.us-east-2.amazonaws.com/harry.txt',
-    harry_text
+    "https://bites-data.s3.us-east-2.amazonaws.com/harry.txt", harry_text
 )
 
 
 def get_harry_most_common_word():
-    pass
+    """Find most common word in Harry Potter text."""
+    # get Harry Potter text, lower-case.
+    with open(harry_text, encoding="utf-8") as f_in:
+        text = f_in.read()
+    text = re.sub(r"[^\w\s]+", "", text).lower()
+    words = text.split()
+    # remove stopwords
+    with open(stopwords_file, encoding="utf-8") as f_in:
+        stopwords = f_in.read().splitlines()
+    words = [word for word in words if word not in stopwords]
+    word_freqs = Counter(words)
+    return word_freqs.most_common()[0]
