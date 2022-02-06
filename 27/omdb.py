0ca093b1427a39ba5e0b235110e0549d64869179
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-06 07:20:44 -0700

    janitorial work

--- 27/omdb.py
+++ 27/omdb.py
@@ -1,21 +1,42 @@
+#!/usr/bin/env python3
+"""Bite 27. Parse omdb movie json data."""
+
 import json
+import os
+from pathlib import Path
+from typing import Dict, List
+from urllib.request import urlretrieve
 
 
-def get_movie_data(files: list) -> list:
-    """Parse movie json files into a list of dicts"""
-    pass
+def retrieve_json():
+    """Retrieve the json to see what it looks like."""
+    tmp = Path(os.getenv("TMP", "/tmp"))
+    s3 = "https://bites-data.s3.us-east-2.amazonaws.com/"
+    data = "omdb_data"
+
+    data_local = tmp / data
+    if not Path(data_local).exists():
+        urlretrieve(s3 + data, data_local)
+
+
+def get_movie_data(files: list) -> List[Dict]:
+    """Parse movie json files into a list of dicts."""
+    return []
 
 
 def get_single_comedy(movies: list) -> str:
-    """return the movie with Comedy in Genres"""
-    pass
+    """Return the movie with Comedy in Genres."""
+    return ""
 
 
 def get_movie_most_nominations(movies: list) -> str:
-    """Return the movie that had the most nominations"""
-    pass
+    """Return the movie that had the most nominations."""
+    return ""
 
 
 def get_movie_longest_runtime(movies: list) -> str:
-    """Return the movie that has the longest runtime"""
-    pass
+    """Return the movie that has the longest runtime."""
+    return ""
+
+if __name__ == "__main__":
+    print("hello")
