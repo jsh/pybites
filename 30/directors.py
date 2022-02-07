fc9889bbde5669f5819e1c0817acda1b7375c5d2
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-07 15:51:18 -0700

    submission Bite 30 @ codechalleng.es

--- 30/directors.py
+++ 30/directors.py
@@ -3,9 +3,8 @@
 import csv
 import os
 from collections import defaultdict, namedtuple
-from typing import Dict, List, Tuple
+from typing import Dict, List, Sequence
 from urllib.request import urlretrieve
-import sys
 
 BASE_URL = "https://bites-data.s3.us-east-2.amazonaws.com/"
 TMP = os.getenv("TMP", "/tmp")
@@ -22,22 +21,27 @@ MIN_YEAR = 1960
 Movie = namedtuple("Movie", "title year score")
 
 
-def get_movies_by_director() -> Dict[str, Movie]:
+def get_movies_by_director() -> Dict[str, List[Movie]]:
     """Convert from csv to dict, indexed by director.
 
     Extracts all movies from csv and stores them in a dict,
     where keys are directors, and values are a list of movies,
     use the defined Movie namedtuple
     """
-    import csv
-    movies = defaultdict(list)
-    with open(LOCAL, newline='', encoding="utf-8") as csvfile:
-        reader = csv.DictReader(csvfile, quotechar='|')
+    directors = defaultdict(list)
+    with open(LOCAL, newline="", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
         for row in reader:
-            director = row["director_name"]
-            movie = Movie(row["movie_title"], row["title_year"], row["imdb_score"])
-            movies[director].append(movie)
-    return movies
+            name = row["director_name"]
+            try:
+                movie = Movie(
+                    row["movie_title"], int(row["title_year"]), float(row["imdb_score"])
+                )
+                if movie.year >= MIN_YEAR:
+                    directors[name].append(movie)
+            except ValueError:
+                pass
+    return directors
 
 
 def calc_mean_score(movies: List[Movie]) -> float:
@@ -46,10 +50,11 @@ def calc_mean_score(movies: List[Movie]) -> float:
     Helper method to calculate mean of list of Movie namedtuples,
     round the mean to 1 decimal place
     """
-    return 0.0
+    mean = sum([movie.score for movie in movies]) / len(movies)
+    return round(mean, 1)
 
 
-def get_average_scores(directors: Dict[str, Movie]) -> List[Tuple[str, float]]:
+def get_average_scores(directors: Dict[str, List[Movie]]) -> Sequence:
     """Make list of directors by score.
 
     Iterate through the directors dict (returned by get_movies_by_director),
@@ -57,4 +62,10 @@ def get_average_scores(directors: Dict[str, Movie]) -> List[Tuple[str, float]]:
     score in descending order. Only take directors into account
     with >= MIN_MOVIES
     """
-    return []
+    scores = [
+        (name, calc_mean_score(movies))
+        for name, movies in directors.items()
+        if len(movies) >= MIN_MOVIES
+    ]
+    scores = sorted(scores, key=lambda x: x[1], reverse=True)
+    return scores
