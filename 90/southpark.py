cb3aa8e2e222ef6a0a39ef514d0fc80b8b65219a
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-08 09:10:06 -0700

    add typing

--- 90/southpark.py
+++ 90/southpark.py
@@ -1,30 +1,33 @@
 """Bite 90. What South Park characters talk most?"""
-# TODO: figure out parameter and return types
 
 import csv
 from collections import Counter, defaultdict
+from typing import Dict
 
 import requests
 
 CSV_URL = "https://raw.githubusercontent.com/pybites/SouthParkData/master/by-season/Season-{}.csv"  # noqa E501
 
 
-def get_season_csv_file(season):
+def get_season_csv_file(season: int) -> str:
     """Download season CSV.
 
     Receives a season int, and downloads loads in its
     corresponding CSV_URL
     """
-    with requests.Session() as s:
-        download = s.get(CSV_URL.format(season))
+    with requests.Session() as session:
+        download = session.get(CSV_URL.format(season))
         return download.content.decode("utf-8")
 
 
-def get_num_words_spoken_by_character_per_episode(content):
-    """Count words spoken gy character per episode.
+def get_num_words_spoken_by_character_per_episode(content: str) -> Dict[str, Counter]:
+    """Count words spoken by character per episode.
 
     Receives loaded csv content (str) and returns a dict of
     keys=characters and values=Counter object,
     which is a mapping of episode=>words spoken
     """
-    pass
+    words_per_episode: Counter = Counter()
+    words_per_episode["1"] = 6
+    words_per_episode["5"] = 9
+    return {"Cartman": words_per_episode}
