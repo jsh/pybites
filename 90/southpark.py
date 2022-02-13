b72471d060b160b3e2276d8521c3d38bb635f1db
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-13 07:20:48 -0700

    stumbling towards correctness

--- 90/southpark.py
+++ 90/southpark.py
@@ -20,14 +20,27 @@ def get_season_csv_file(season: int) -> str:
         return download.content.decode("utf-8")
 
 
-def get_num_words_spoken_by_character_per_episode(content: str) -> Dict[str, Counter]:
+def get_num_words_spoken_by_character_per_episode(
+    content: str,
+) -> Dict[str, Counter[str]]:
     """Count words spoken by character per episode.
 
     Receives loaded csv content (str) and returns a dict of
     keys=characters and values=Counter object,
     which is a mapping of episode=>words spoken
     """
-    words_per_episode: Counter = Counter()
-    words_per_episode["1"] = 6
-    words_per_episode["5"] = 9
-    return {"Cartman": words_per_episode}
+    lines = content.splitlines()
+
+    speech_by_character = defaultdict(list)
+    for line in lines:
+        _, episode, character, speech = line.split(",")
+        speech_by_character[character].append((episode, len(speech.split())))
+
+    words_by_character = {}
+    for character in speech_by_character:
+        words_per_episode = defaultdict(int)
+        for episode, words in speech_by_character[character]:
+            words_per_episode[episode] += words
+        words_by_character[character] = words_per_episode
+
+    return words_by_character
