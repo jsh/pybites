1d338b51ea3b1078f6f558a79ad1c17e465a9943
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-15 20:24:24 -0700

    Janitorial work.

--- 178/commits.py
+++ 178/commits.py
@@ -1,22 +1,24 @@
-from collections import Counter
+"""Bite 178. Parse PyBites blog git commit log."""
+
 import os
+from collections import Counter
+from typing import Tuple
 from urllib.request import urlretrieve
 
 from dateutil.parser import parse
 
-commits = os.path.join(os.getenv("TMP", "/tmp"), 'commits')
-urlretrieve(
-    'https://bites-data.s3.us-east-2.amazonaws.com/git_log_stat.out',
-    commits
-)
+commits = os.path.join(os.getenv("TMP", "/tmp"), "commits")
+urlretrieve("https://bites-data.s3.us-east-2.amazonaws.com/git_log_stat.out", commits)
 
 # you can use this constant as key to the yyyymm:count dict
-YEAR_MONTH = '{y}-{m:02d}'
+YEAR_MONTH = "{y}-{m:02d}"
 
 
-def get_min_max_amount_of_commits(commit_log: str = commits,
-                                  year: int = None) -> (str, str):
-    """
+def get_min_max_amount_of_commits(
+    commit_log: str = commits, year: int = None
+) -> Tuple[str, str]:
+    """Get the months with fewest and most commits.
+
     Calculate the amount of inserts / deletes per month from the
     provided commit log.
 
@@ -25,4 +27,3 @@ def get_min_max_amount_of_commits(commit_log: str = commits,
 
     Returns a tuple of (least_active_month, most_active_month)
     """
-    pass
