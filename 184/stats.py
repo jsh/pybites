3946430e8156e71ba2594ef0798bfd99eb503da7
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-17 11:53:04 -0700

    Janitorial work

--- 184/stats.py
+++ 184/stats.py
@@ -1,51 +1,52 @@
-from csv import DictReader
+"""Bite 184. Analyze some Bite stats data."""
+
 import os
+from csv import DictReader
 from urllib.request import urlretrieve
 
 TMP = os.getenv("TMP", "/tmp")
-LOGS = 'bite_output_log.txt'
+LOGS = "bite_output_log.txt"
 DATA = os.path.join(TMP, LOGS)
-S3 = 'https://bites-data.s3.us-east-2.amazonaws.com'
+S3 = "https://bites-data.s3.us-east-2.amazonaws.com"
 if not os.path.isfile(DATA):
-    urlretrieve(f'{S3}/{LOGS}', DATA)
+    urlretrieve(f"{S3}/{LOGS}", DATA)
 
 
 class BiteStats:
+    """Get statistics on Pybites data."""
 
     def _load_data(self, data) -> list:
-        pass  # start here
+        """Resolve data into rows."""
+        return []
 
     def __init__(self, data=DATA):
+        """Save data in instance local."""
         self.rows = self._load_data(data)
 
     @property
     def number_bites_accessed(self) -> int:
-        """Get the number of unique Bites accessed"""
-        pass
+        """Get the number of unique Bites accessed."""
 
     @property
     def number_bites_resolved(self) -> int:
-        """Get the number of unique Bites resolved (completed=True)"""
-        pass
+        """Get the number of unique Bites resolved (completed=True)."""
 
     @property
     def number_users_active(self) -> int:
-        """Get the number of unique users in the data set"""
-        pass
+        """Get the number of unique users in the data set."""
 
     @property
     def number_users_solving_bites(self) -> int:
-        """Get the number of unique users that resolved
-           one or more Bites"""
-        pass
+        """Get the number of unique users that resolved one or more Bites."""
 
     @property
     def top_bite_by_number_of_clicks(self) -> str:
-        """Get the Bite that got accessed the most
-           (= in most rows)"""
-        pass
+        """Get the Bite that got accessed the most (= in most rows)."""
 
     @property
     def top_user_by_bites_completed(self) -> str:
-        """Get the user that completed the most Bites"""
-        pass
+        """Get the user that completed the most Bites."""
+
+
+if __name__ == "__main__":
+    print(BiteStats().rows)
