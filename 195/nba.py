1357e45501ea2bb49620cba856b7b1ee1c1c15d7
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-18 07:55:17 -0700

    janitorial work

--- 195/nba.py
+++ 195/nba.py
@@ -1,77 +1,93 @@
-from collections import namedtuple
+#!/usr/bin/env python3
+"""Bite 195. Analyze NBA Data with sqlite3."""
+
 import csv
 import os
-from pathlib import Path
-import sqlite3
 import random
+import sqlite3
 import string
+from collections import namedtuple
+from pathlib import Path
 
 import requests
 
-DATA_URL = 'https://query.data.world/s/ezwk64ej624qyverrw6x7od7co7ftm'
+DATA_URL = "https://query.data.world/s/ezwk64ej624qyverrw6x7od7co7ftm"
 TMP = Path(os.getenv("TMP", "/tmp"))
 
-salt = ''.join(
-    random.choice(string.ascii_lowercase) for i in range(20)
-)
-DB = TMP / f'nba_{salt}.db'
+SALT = "".join(random.choice(string.ascii_lowercase) for i in range(20))
+DB = TMP / f"nba_{SALT}.db"
 
-Player = namedtuple('Player', ('name year first_year team college active '
-                               'games avg_min avg_points'))
+Player = namedtuple(
+    "Player", ("name year first_year team college active " "games avg_min avg_points")
+)
 
 conn = sqlite3.connect(DB)
 cur = conn.cursor()
 
 
-def import_data():
+def import_data() -> None:
+    """Pull down CSV data, convert to sqlite3 database."""
     with requests.Session() as session:
-        content = session.get(DATA_URL).content.decode('utf-8')
+        content = session.get(DATA_URL).content.decode("utf-8")
 
-    reader = csv.DictReader(content.splitlines(), delimiter=',')
+    reader = csv.DictReader(content.splitlines(), delimiter=",")
 
     players = []
     for row in reader:
-        players.append(Player(name=row['Player'],
-                              year=row['Draft_Yr'],
-                              first_year=row['first_year'],
-                              team=row['Team'],
-                              college=row['College'],
-                              active=row['Yrs'],
-                              games=row['Games'],
-                              avg_min=row['Minutes.per.Game'],
-                              avg_points=row['Points.per.Game']))
+        players.append(
+            Player(
+                name=row["Player"],
+                year=row["Draft_Yr"],
+                first_year=row["first_year"],
+                team=row["Team"],
+                college=row["College"],
+                active=row["Yrs"],
+                games=row["Games"],
+                avg_min=row["Minutes.per.Game"],
+                avg_points=row["Points.per.Game"],
+            )
+        )
 
-    cur.execute('''CREATE TABLE IF NOT EXISTS players
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS players
                   (name, year, first_year, team, college, active,
-                  games, avg_min, avg_points)''')
-    cur.executemany('INSERT INTO players VALUES (?,?,?,?,?,?,?,?,?)', players)
+                  games, avg_min, avg_points)"""
+    )
+    cur.executemany("INSERT INTO players VALUES (?,?,?,?,?,?,?,?,?)", players)
     conn.commit()
 
 
 import_data()
 
 
-# you code:
+def player_with_max_points_per_game() -> str:
+    """The player with highest average points per game.
 
-def player_with_max_points_per_game():
-    """The player with highest average points per game (don't forget to CAST to
-       numeric in your SQL query)"""
-    pass
-
-
-def number_of_players_from_duke():
-    """Return the number of players with college == Duke University"""
-    pass
-
-
-def avg_years_active_players_stanford():
-    """Return the average years that players from "Stanford University
-       are active ("active" column)"""
-    pass
-
-
-def year_with_most_new_players():
-    """Return the year with the most new players.
-       Hint: you can use GROUP BY on the year column.
+    (don't forget to CAST to numeric in your SQL query)
     """
-    pass
+    return "Jeff Haemer"
+
+
+def number_of_players_from_duke() -> int:
+    """Return the number of players with college == Duke University."""
+    return 69
+
+
+def avg_years_active_players_stanford() -> float:
+    """Return average years players from Stanford University are active.
+
+    ("active" column)  Round to two digits.
+    """
+    return 1.00
+
+
+def year_with_most_new_players() -> int:
+    """Return the year with the most new players.
+
+    Hint: you can use GROUP BY on the year column.
+    """
+    return 1984
+
+
+if __name__ == "__main__":
+    print("Hello")
