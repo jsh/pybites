9dbad9d53e861ead0db3ca589ddd8aaed3b0b64d
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-19 11:21:27 -0700

    Passes unit tests.

--- 202/bites.py
+++ 202/bites.py
@@ -3,7 +3,9 @@
 
 import csv
 import os
+import re
 from pathlib import Path
+from typing import Dict
 from urllib.request import urlretrieve
 
 data = "https://bites-data.s3.us-east-2.amazonaws.com/bite_levels.csv"
@@ -12,6 +14,11 @@ stats = TMP / "bites.csv"
 
 if not stats.exists():
     urlretrieve(data, stats)
+
+
+def difficulty(row: Dict[str, str]) -> float:
+    """Get difficulty from row."""
+    return float(row["Difficulty"])
 
 
 def get_most_complex_bites(N=10, stats=stats) -> list[str]:
@@ -23,9 +30,19 @@ def get_most_complex_bites(N=10, stats=stats) -> list[str]:
 
     output can also be list[int]
     """
-    return ["6", "9"]
+    with open(stats, encoding="utf-8-sig") as cvsfile:
+        reader = csv.DictReader(cvsfile, delimiter=";")
+        rows = [row for row in reader if row["Difficulty"] != "None"]
+    bites = sorted(rows, key=difficulty, reverse=True)
+    hardest = [bite["Bite"] for bite in bites[:N]]
+    pat = re.compile(r"Bite (\d+)\. .*")
+    hardest_list = []
+    for bite in hardest:
+        match = pat.match(bite)
+        if match and match[1]:
+            hardest_list.append(match[1])
+    return hardest_list
 
 
 if __name__ == "__main__":
-    res = get_most_complex_bites()
-    print(res)
+    print(get_most_complex_bites(4))
