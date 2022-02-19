9956c415c09e5455558553f5b1d741e1350b5482
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-19 09:46:11 -0700

    unit tests

--- git/test_commits.py
+++ git/test_commits.py
@@ -1,6 +1,7 @@
 from commits import clone_from_remote, git_dir_present, count_commits, first_commit
 from pathlib import Path
 import pytest
+import shutil
 from git import Repo
 
 URL = "https://github.com/git/git.git"  # or something more fixed
@@ -11,9 +12,17 @@ def local_repo() -> None:
     local = cwd / "local"
     return clone_from_remote(URL, local)
 
+
 @pytest.fixture(scope="module")
 def list_commits(local_repo) -> None:
     return list(local_repo.iter_commits("master"))
+
+
+def test_retrieve_new_repo():
+    cwd = Path.cwd()
+    local = cwd / "local"
+    shutil.rmtree(local)
+    return clone_from_remote(URL, local)
 
 
 def test_retrieve_repo(local_repo) -> None:
