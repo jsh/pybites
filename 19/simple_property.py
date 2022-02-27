e70e9251f8c311a8a43862970368ba4f02f57fa4
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-26 19:14:08 -0700

    submission Bite 19 @ codechalleng.es

--- 19/simple_property.py
+++ 19/simple_property.py
@@ -1,7 +1,19 @@
+"""Bite 19. Write a property."""
+
 from datetime import datetime
 
 NOW = datetime.now()
 
 
 class Promo:
-    pass
+    """Represent a promotion."""
+
+    def __init__(self, name: str, expires: datetime):
+        """Create a Promo object."""
+        self._name = name
+        self._expires = expires
+
+    @property
+    def expired(self) -> bool:
+        """Determine whether the Promo has expired."""
+        return self._expires < NOW
