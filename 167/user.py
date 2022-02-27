a840421e5b726533df5abeff045b4eb52d4a0a47
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-27 09:47:30 -0700

    commit initial + janitorial work.

---
+++ 167/user.py
@@ -0,0 +1,43 @@
+"""Module to implement a User class."""
+
+
+class User:
+    """A User class.
+
+    (Django's User model inspired us)
+    """
+
+    def __init__(self, first_name, last_name):
+        """Construct object with base values."""
+        self.first_name = first_name
+        self.last_name = last_name
+
+    @property
+    def get_full_name(self) -> str:
+        """Return first and last separated by a whitespace.
+
+        Use title case for both.
+        """
+
+    @property
+    def username(self) -> str:
+        """Return username.
+
+        A username consists of the first char of
+        the user's first_name and the first 7 chars
+        of the user's last_name, both lowercased.
+
+        If this is your first property, check out:
+        https://pybit.es/property-decorator.html
+        """
+
+    def __str__(self) -> str:
+        """Return printable version of object."""
+
+    def __repr__(self) -> str:
+        """
+        Return technical version of object.
+
+        Don't hardcode the class name, hint: use a
+        special attribute of self.__class__ ...
+        """
