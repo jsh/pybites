93339d6886c8e33a47eec1fffa29a89d21ddfa11
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-27 18:46:25 -0700

    submission Bite 71 @ codechalleng.es

--- 71/record.py
+++ 71/record.py
@@ -3,3 +3,13 @@
 
 class RecordScore:
     """Class to track a game's maximum score."""
+
+    def __init__(self):
+        """Create RecordScore object."""
+        self.record_score = None
+
+    def __call__(self, score: int) -> int:
+        """If score is higher than current record_score, reset to score."""
+        if (self.record_score is None) or (score > self.record_score):
+            self.record_score = score
+        return self.record_score
