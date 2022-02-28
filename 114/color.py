bb967975a8dce1bbb4372771a4f601f0b9f1576c
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-28 12:26:52 -0700

    submission Bite 114 @ codechalleng.es

--- 114/color.py
+++ 114/color.py
@@ -23,24 +23,41 @@ class Color:
     Take the string of a color name and return its RGB value.
     """
 
+    color_dict = COLOR_NAMES.copy()
+
     def __init__(self, color: str):
         """Create object."""
         self._color = color
+        key = self._color.upper()
+        self.rgb = self.color_dict[key] if key in self.color_dict else None
 
     @staticmethod
-    def hex2rgb(hex: int) -> Tuple[int, int, int]:
+    def hex2rgb(hex_str: str) -> Tuple[int, int, int]:
         """Class method that converts a hex value into an rgb one."""
-        return (0, 0, 0)
+        first = int(hex_str[1:3], 16)
+        second = int(hex_str[3:5], 16)
+        third = int(hex_str[5:7], 16)
+        return (first, second, third)
 
     @staticmethod
     def rgb2hex(rgb: Tuple[int, int, int]) -> str:
         """Class method that converts an rgb value into a hex one."""
-        return "#000000"
+        if not isinstance(rgb, tuple) or len(rgb) != 3:
+            raise ValueError
+        hex_str = "#"
+        for rgb_val in rgb:
+            if 0 <= rgb_val <= 255:
+                hex_str += f"{rgb_val:02x}"
+            else:
+                raise ValueError(f"expected 0 <= {rgb_val} <= 255")
+        return hex_str
 
     def __repr__(self) -> str:
         """Return the repl of the object."""
-        return f'{self.__class__.__name__}("{self._color}")'
+        return f"{self.__class__.__name__}('{self._color}')"
 
     def __str__(self) -> str:
         """Return the string value of the color object."""
-        return self._color
+        if self.rgb:
+            return str(self.rgb)
+        return "Unknown"
