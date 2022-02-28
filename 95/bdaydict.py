55679cbe31136c7b23e85b51622d984379c6fc2b
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-02-28 08:59:56 -0700

    submission Bite 95 @ codechalleng.es

--- 95/bdaydict.py
+++ 95/bdaydict.py
@@ -1,9 +1,24 @@
-MSG = 'Hey {}, there are more people with your birthday!'
+"""Bite 95. Subclass the dict built-in."""
+
+MSG = "Hey {}, there are more people with your birthday!"
 
 
 class BirthdayDict(dict):
-    """Override dict to print a message every time a new person is added that has
-       the same birthday (day+month) as somebody already in the dict"""
+    """Dict that announces duplicates.
 
-    def __setitem__(self, name, birthday):
-        pass
+    Override dict to print a message every time a new person is added that has
+    the same birthday (day+month) as somebody already in the dict
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Create BirthdayDict object."""
+        dict.__init__(self, *args, **kwargs)
+
+    def __setitem__(self, name, birthday) -> None:
+        """Add an entry to the dict."""
+        month = birthday.month
+        day = birthday.day
+        for date in self.values():
+            if date.month == month and date.day == day:
+                print(MSG.format(name))
+        dict.__setitem__(self, name, birthday)
