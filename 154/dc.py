40dad5a9de545abd386affe48af1b8d343b37ed7
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-03-02 08:08:41 -0700

    submission Bite 154 @ codechalleng.es

--- 154/dc.py
+++ 154/dc.py
@@ -1,1 +1,16 @@
+"""Bite 154. Write your own Data Class."""
+
 from dataclasses import dataclass
+
+
+@dataclass(order=True)
+class Bite:
+    """Class for keeping track of a Bite."""
+
+    number: int
+    title: str
+    level: str = "Beginner"
+
+    def __post_init__(self):
+        """Capitalize Bite title."""
+        self.title = self.title.capitalize()
