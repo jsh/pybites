7da5d3b7c61949c7c89c9dd3472a53b18222ea23
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-03-02 13:06:37 -0700

    submission Bite 199 @ codechalleng.es

--- 199/person.py
+++ 199/person.py
@@ -1,1 +1,35 @@
+"""Bite 199. Multiple inheritance (__mro__)."""
 # see __mro__ output in Bite description
+
+
+class Person:
+    """Represent a person."""
+
+    def __repr__(self) -> str:
+        """Represent the object."""
+        identity = "I am a person"
+        return identity
+
+
+class Father(Person):
+    """Represent a father."""
+
+    def __repr__(self) -> str:
+        """Represent the object."""
+        return super().__repr__() + " and cool daddy"
+
+
+class Mother(Person):
+    """Represent a mother."""
+
+    def __repr__(self) -> str:
+        """Represent the object."""
+        return super().__repr__() + " and awesome mom"
+
+
+class Child(Father, Mother):
+    """Represent a child."""
+
+    def __repr__(self) -> str:
+        """Represent the object."""
+        return "I am the coolest kid"
