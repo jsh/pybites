9b8e29dea90ae5559ae07b2d84fe83d97dbe999d
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-03-02 08:15:47 -0700

    Janitorial work.

--- 166/ini.py
+++ 166/ini.py
@@ -1,26 +1,28 @@
+"""Bite 166. Complete a tox ini file parser class."""
+
 import configparser
 
 
 class ToxIniParser:
+    """Parser for Tox ini file."""
 
     def __init__(self, ini_file):
-        """Use configparser to load ini_file into self.config"""
-        pass
+        """Use configparser to load ini_file into self.config."""
 
     @property
     def number_of_sections(self):
         """Return the number of sections in the ini file.
-           New to properties? -> https://pybit.es/property-decorator.html
+
+        New to properties? -> https://pybit.es/property-decorator.html
         """
-        pass
 
     @property
     def environments(self):
-        """Return a list of environments
-           (= "envlist" attribute of [tox] section)"""
-        pass
+        """Return a list of environments.
+
+        (= "envlist" attribute of [tox] section)
+        """
 
     @property
     def base_python_versions(self):
-        """Return a list of all basepython across the ini file"""
-        pass
+        """Return a list of all basepython across the ini file."""
