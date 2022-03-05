e71fd91ed58011ce2be31be13214f6bab3f67d77
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-03-05 09:00:37 -0700

    submission Bite 313 @ codechalleng.es

--- 313/constructors.py
+++ 313/constructors.py
@@ -10,7 +10,7 @@ class DomainException(Exception):
 class Domain:
     """Class to hold a domain name."""
 
-    def __init__(self, name):
+    def __init__(self, name: str):
         """Construct Domain object."""
         # validate a current domain (r'.*\.[a-z]{2,3}$' is fine)
         # if not valid, raise a DomainException
@@ -24,6 +24,24 @@ class Domain:
     # called parse_from_url and parse_from_email to construct domains
     # from an URL and email respectively
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return string representing the domain."""
         return self.name
+
+    @classmethod
+    def parse_url(cls, url: str) -> "Domain":
+        """Parse a URL with http(s):// ."""
+        pat = re.compile(r"https?://([^/]+).*$")
+        match = pat.match(url)
+        if match:
+            return cls(match[1])
+        return cls("")
+
+    @classmethod
+    def parse_email(cls, email: str) -> "Domain":
+        """Parse a URL with name@url."""
+        pat = re.compile(r"[^@]+@(\S+)$")
+        match = pat.match(email)
+        if match:
+            return cls(match[1])
+        return cls("")
