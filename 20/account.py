025f0a42d0ce6215c2e8e948b98094e2b699df38
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-03-05 20:49:07 -0700

    janitorial work.

--- 20/account.py
+++ 20/account.py
@@ -1,17 +1,25 @@
+"""Bite 20. Write a context manager."""
+
+
 class Account:
+    """Represent an account with transactions."""
 
     def __init__(self):
+        """Create the object."""
         self._transactions = []
 
     @property
-    def balance(self):
+    def balance(self) -> int:
+        """Report the balance."""
         return sum(self._transactions)
 
-    def __add__(self, amount):
+    def __add__(self, amount: int):
+        """Add a transaction."""
         self._transactions.append(amount)
 
-    def __sub__(self, amount):
+    def __sub__(self, amount: int):
+        """Subtract a transaction."""
         self._transactions.append(-amount)
 
-    # add 2 dunder methods here to turn this class 
+    # add 2 dunder methods here to turn this class
     # into a 'rollback' context manager
