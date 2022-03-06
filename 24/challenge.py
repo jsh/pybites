173201aac66e502b7c45e52b35f2590130cb6ee4
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-03-06 11:33:54 -0700

    janitorial work.

--- 24/challenge.py
+++ 24/challenge.py
@@ -1,13 +1,38 @@
+"""Bite 24. ABC's and class inheritance."""
 from abc import ABC, abstractmethod
+from typing import List
 
 
 class Challenge(ABC):
-    pass
+    """Keep track of a challenge number name."""
+
+    def __init__(self, number: int, title: str) -> None:
+        """Construct the abstract object."""
+        self.number = number
+        self.title = title
+
+    @abstractmethod
+    def verify(self):
+        """method docstring."""
+
+    @property
+    def pretty_title(self):
+        """property docstring."""
 
 
 class BlogChallenge(Challenge):
-    pass
+    """A Challenge with merged prs."""
+
+    def __init__(self, number: int, title: str, prs: List[int]) -> None:
+        """Create the object."""
+        super().__init__(number, title)
+        self.prs = prs
 
 
 class BiteChallenge(Challenge):
-    pass
+    """Another Challenge with merged prs."""
+
+    def __init__(self, number: int, title: str, prs: List[int]) -> None:
+        """Create the object."""
+        super().__init__(number, title)
+        self.prs = prs
