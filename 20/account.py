bc0b28ec397fd3440daddaae3b0affc8f10d0ba0
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-03-06 10:54:24 -0700

    submission Bite 20 @ codechalleng.es

--- 20/account.py
+++ 20/account.py
@@ -1,25 +1,37 @@
 """Bite 20. Write a context manager."""
 
+from typing import List
+
 
 class Account:
     """Represent an account with transactions."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Create the object."""
-        self._transactions = []
+        self._transactions: List[int] = []
 
     @property
     def balance(self) -> int:
         """Report the balance."""
         return sum(self._transactions)
 
-    def __add__(self, amount: int):
+    def __add__(self, amount: int) -> None:
         """Add a transaction."""
         self._transactions.append(amount)
 
-    def __sub__(self, amount: int):
+    def __sub__(self, amount: int) -> None:
         """Subtract a transaction."""
         self._transactions.append(-amount)
 
     # add 2 dunder methods here to turn this class
     # into a 'rollback' context manager
+
+    def __enter__(self) -> "Account":
+        """Save transaction history on entrance to context mgr."""
+        self._save_transactions = self._transactions.copy()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        """Restore transaction history on context mgr exit if balance < 0."""
+        if self.balance < 0:
+            self._transactions = self._save_transactions
