c8f63a2519d1f3d1cb2f67ba3250fd9204b4592d
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-03-07 16:51:03 -0700

    Janitorial work.

--- 34/karma.py
+++ 34/karma.py
@@ -1,10 +1,36 @@
+"""Bite 34. Building a Karma app - implement the User class."""
+
 from collections import namedtuple
 from datetime import datetime
+from typing import List
 
-Transaction = namedtuple('Transaction', 'giver points date')
+Transaction = namedtuple("Transaction", "giver points date")
 # https://twitter.com/raymondh/status/953173419486359552
 Transaction.__new__.__defaults__ = (datetime.now(),)
 
 
 class User:
-    pass
+    """Represent a user object."""
+
+    def __init__(self, name: str) -> None:
+        """Create the object."""
+        self.name = name
+        self.karma = 0
+        self._transactions: List[int] = []
+
+    @property
+    def points(self) -> List[int]:
+        """List karma points."""
+        return self._transactions
+
+    @property
+    def fans(self) -> int:
+        """Count fans."""
+        return 0  # TODO: what is this?
+
+    def __add__(self, trans: Transaction) -> None:
+        """Add transactions."""
+
+    def __str__(self) -> str:
+        """String representation of User."""
+        return f"{self.name} has a karma of {self.points} and {self.fans} fans"
