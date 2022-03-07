32b0cdc64f6affd3f6a755bab5359e3fae866445
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-03-07 07:15:13 -0700

    Sanity checks on object, function args.
    Add properties to calculate row and column lengths.
    Verify that all rows are the same length.
    Verify that matrices being multiplied are the right shape.
    Verify that Matrix is a 2-D array of ints.
    
    This should keep me from making some dumb errors.

--- 31/matmul.py
+++ 31/matmul.py
@@ -3,13 +3,51 @@
 from typing import List
 
 
+class MatrixError(Exception):
+    """Matrix object doesn't have the right shape."""
+
+
 class Matrix(object):
     """Implement matrix object with multiplication."""
 
     def __init__(self, values: List[List[int]]) -> None:
         """Create Matrix object."""
         self.values = values
+        self.validate()
+
+    @property
+    def row(self) -> int:
+        return len(self.values)
+
+    @property
+    def col(self) -> int:
+        return len(self.values[0])
+
+    def validate(self) -> None:
+        ncol = self.col
+        for row in self.values:
+            try:
+                len(row) == ncol
+            except:
+                raise MatrixError(f"{row} must have {ncol} elements")
+            for elem in row:
+                try:
+                    isinstance(elem, int)
+                except:
+                    raise MatrixError(f"{elem} must be int")
 
     def __repr__(self) -> str:
         """Show the contents."""
         return f'<Matrix values="{self.values}">'
+
+    def __matmul__(self, other: List[List[int]]) -> List[List[int]]:
+        """Multiply two matrices."""
+        assert self.col == other.row, f"{self.col} must equal {other.row}"
+
+    def __rmatmul__(self, other: List[List[int]]) -> List[List[int]]:
+        """Multiply two matrices."""
+        assert self.col == other.row, f"{self.col} must equal {other.row}"
+
+    def __imatmul__(self, other: List[List[int]]) -> List[List[int]]:
+        """Multiply two matrices."""
+        assert self.col == other.row, f"{self.col} must equal {other.row}"
