0254faa9dae7a259a9b7d82d0525abe6765a91c5
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-03-08 08:13:43 -0700

    submission Bite 34 @ codechalleng.es

--- 34/karma.py
+++ 34/karma.py
@@ -4,9 +4,10 @@ from collections import namedtuple
 from datetime import datetime
 from typing import List
 
-Transaction = namedtuple("Transaction", "giver points date")
+# Transaction = namedtuple("Transaction", "giver points date")
 # https://twitter.com/raymondh/status/953173419486359552
-Transaction.__new__.__defaults__ = (datetime.now(),)
+# Transaction.__new__.__defaults__ = (datetime.now(),)
+Transaction = namedtuple("Transaction", "giver points date", defaults=(datetime.now(),))
 
 
 class User:
@@ -16,21 +17,26 @@ class User:
         """Create the object."""
         self.name = name
         self.karma = 0
-        self._transactions: List[int] = []
+        self._transactions: List[Transaction] = []
 
     @property
     def points(self) -> List[int]:
         """List karma points."""
-        return self._transactions
+        return [transaction.points for transaction in self._transactions]
 
     @property
     def fans(self) -> int:
         """Count fans."""
-        return 0  # TODO: what is this?
+        fans = {transaction.giver for transaction in self._transactions}
+        return len(fans)
 
-    def __add__(self, trans: Transaction) -> None:
+    def __add__(self, transaction: Transaction) -> None:
         """Add transactions."""
+        self._transactions.append(transaction)
+        self.karma += transaction.points
 
     def __str__(self) -> str:
         """String representation of User."""
-        return f"{self.name} has a karma of {self.points} and {self.fans} fans"
+        return f"{self.name} has a karma of {self.karma} and {self.fans} fan" + (
+            "s" if self.fans > 1 else ""
+        )
