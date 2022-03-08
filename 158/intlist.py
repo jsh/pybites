96fccdda4c38b5568ee3dc4e4da3fe28a32038c7
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-03-08 11:47:26 -0700

    Janitorial pass.

--- 158/intlist.py
+++ 158/intlist.py
@@ -1,2 +1,17 @@
+"""Bite 158. Subclass the list built-in."""
+
+from typing import List
+
+
 class IntList(list):
-    pass
+    """Lists of integers that understand mean() and median().
+
+    Raise TypeError for non-int values.
+    """
+
+    def __init__(self, elements: List[int] = []) -> None:
+        """Create the object."""
+        for elem in elements:
+            if not isinstance(elem, int):
+                raise TypeError
+        super().__init__(elements)
