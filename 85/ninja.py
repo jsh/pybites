aeeb91e327b608238b85b03f1a53a8f567e02987
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-03-08 08:36:08 -0700

    janitorial work

--- 85/ninja.py
+++ 85/ninja.py
@@ -1,22 +1,27 @@
+"""Bite 85. Write a score property."""
+
 scores = [10, 50, 100, 175, 250, 400, 600, 800, 1000]
-ranks = 'white yellow orange green blue brown black paneled red'.split()
+ranks = "white yellow orange green blue brown black paneled red".split()
 BELTS = dict(zip(scores, ranks))
 
 
 class NinjaBelt:
+    """Keep track of Ninja belts and points."""
 
-    def __init__(self, score=0):
+    def __init__(self, score: int = 0) -> None:
+        """Create the object."""
         self._score = score
         self._last_earned_belt = None
 
-    def _get_belt(self, new_score):
+    def _get_belt(self, new_score: int) -> str:
         """Might be a useful helper"""
-        pass
 
-    def _get_score(self):
-        pass
+    def _get_score(self) -> int:
+        """Get the score."""
 
-    def _set_score(self, new_score):
-        pass
+    def _set_score(self, new_score: int) -> None:
+        """Set a new score."""
 
-    score = property(_get_score, _set_score)
+    score = property(
+        _get_score, _set_score
+    )  # TODO: Oh. This is interesting. Look this up.
